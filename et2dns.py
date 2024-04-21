a13593e48221d7e41ed8e66284808ec1cfb7b5d2
Yuneshwaran <126149253+yuneshwaran@users.noreply.github.com>
2024-04-21 22:01:08 +0530

    Update et2dns.py

--- et2dns.py
+++ et2dns.py
@@ -82,57 +82,7 @@ header = """# (Unofficial) Emerging Threats PiHole blocklist
 # Utility: {}
 # Status: Beta / in development
 # Last modified: {}
-#
-# WHAT:
-# This blocklist is intended for use in PiHole or similar DNS-level filters. It's generated automatically from part of
-# the current Emerging Threats Open ruleset, which is threat intelligence and signatures provided by the Emerging
-# Threats research team and contributed to by security researchers around the world.
-#
-# TECHNICAL NOTICE:
-# While this list provides some DNS filtering coverage, the provided filter is NOT comparable to protection offered by
-# Emerging Threats' signatures when implemented in an IPS such as Snort or Suricata. This is because IDS can perform
-# advanced matching functionality and make bypassing the filter much more difficult. Some key examples:
-#  * If a particular strain of malware queries the public DNS resolver 8.8.8.8 directly, this could bypass PiHole on
-#    your network.
-#  * Emerging Threats includes much more than blocking specific domains, such as detecting and blocking DNS
-#    exfiltration attacks based on different parts of the DNS payload that PiHole would simply ignore.
-#  * And of course, Emerging Threats covers 100s of different protocols with their signatures, extending FAR beyond
-#    DNS! This allows researchers to write very specific rules to detect and block threats at the network level,
-#    making it harder for malware or threats to hide from security staff by just changing what domain they use.
-# After all, a domain can cost only a few dollars - but re-engineering your custom malware implant could take days!
-#
-# WHY:
-# First, of course I hope this can help you keep some malware/unwanted traffic/etc. off your network!
-# Second, for folks interested in cybersecurity (personal or career) that you get a glimpse of some new technology
-# that you may not have heard of before and something fun to learn about - or maybe contribute to in the future! :)
-#
-# SOMETHING IS WRONG:
-# Sorry! This is NOT an official Emerging Threats project and while I'll do my best to ensure correctness,
-# this hosts file is not provided with any guarantees.
-# Please report false positives or other issues here: https://github.com/tweedge/emerging-threats-pihole/issues
-# 
-# LICENSE:
-# Emerging Threats community rules, from which this hosts file is derived, are BSD-licensed:
-#  Copyright (c) 2003-2021, Emerging Threats
-#  All rights reserved.
-#  
-#  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
-#  following conditions are met:
-#  
-#  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following 
-#    disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
-#    following disclaimer in the documentation and/or other materials provided with the distribution.
-#  * Neither the name of the nor the names of its contributors may be used to endorse or promote products derived 
-#    from this software without specific prior written permission.
-#  
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-#  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
-#  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+# Blocklist generated
 """
 
 for category_name, category_content in categories.items():
